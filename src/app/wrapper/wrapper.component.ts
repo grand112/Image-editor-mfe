7250ce379db6b42c7c283acb3b71eed28b622900
Łukasz Bugajski <lukaszbugajski1998@gmail.com>
2022-02-06 21:22:22 +0100

    add build:externals conf

--- src/app/wrapper/wrapper.component.ts
+++ src/app/wrapper/wrapper.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-wrapper',
@@ -7,8 +8,10 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@
   styleUrls: ['./wrapper.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WrapperComponent implements OnInit {
+export class WrapperComponent implements OnInit, OnDestroy {
   defaultFile: File;
+
+  private defaultFileSubscription: Subscription;
 
   constructor(
     private http: HttpClient,
@@ -16,10 +19,13 @@ export class WrapperComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.http.get('assets/default.jpg', { responseType: 'blob' }).subscribe((defaultBlob: Blob) => {
+    this.defaultFileSubscription =  this.http.get('assets/default.jpg', { responseType: 'blob' }).subscribe((defaultBlob: Blob) => {
       this.defaultFile = new File([defaultBlob], 'fileName');
       this.cdr.detectChanges();
     });
   }
 
+  ngOnDestroy(): void {
+    this.defaultFileSubscription.unsubscribe();
+  }
 }
